8fefb22ff04886eb90dbe9cc982e5eda4ee65b7f
orihimee <hamidfars201@gmail.com>
2026-02-19 19:18:06 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "expo-router/babel",
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            "@": "./",
-          },
-        },
+      // Strip console.* calls in production
+      process.env.NODE_ENV === 'production' && [
+        'transform-remove-console',
+        { exclude: [] },
       ],
-    ],
+    ].filter(Boolean),
   };
 };
