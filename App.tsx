3ce976da6952e254db2910b3593cba8baaa805a4
orihimee <hamidfars201@gmail.com>
2026-02-20 01:10:47 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import ChapterScreen from './src/screens/ChapterScreen';
 
 export default function App() {
-  return <View style={{ flex: 1, backgroundColor: '#000' }} />;
+  return <ChapterScreen />;
 }
